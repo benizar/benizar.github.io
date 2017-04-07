67b18974553dd4cf1166411f627a7d9c21c1905c
benizar <benizar@gmail.com>
2017-04-07 13:16:33 +0200

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -17,23 +17,38 @@ tmp_dir="temp_$last_SHA"
 # Make sure on the SOURCE branch
 git checkout $SOURCE > /dev/null 2>&1
 
-
-# Move the Jekyll site to a temporary folder
-cp -r _site/* ~/$tmp_dir/ > /dev/null 2>&1
+# Jekyll build to create _site, then down docker-compose
+docker-compose -f build.yml up > /dev/null 2>&1
 if [ $? = 0 ]; then
-  echo "cp _site to temp successful"
+  docker-compose -f build.yml down
+  echo "Build successful"
 else
-  echo "cp _site to temp failed"
+  docker-compose -f build.yml down
+  echo "Build failed"
   exit 1
 fi
+
+# Copy _site to a temporary folder
+cp -r _site/ ~/$tmp_dir/ > /dev/null 2>&1
+if [ $? = 0 ]; then
+  echo "copy _site to $tmp_dir successful"
+else
+  echo "copy _site to $tmp_dir failed"
+  exit 1
+fi
+
+
+# Delete remote and local $SITE
+git push origin --delete $SITE
+git branch -D $SITE
 
 # Switch to the SITE branch
 git checkout $SITE > /dev/null 2>&1
 if [ $? = 1 ]; then
   # Branch does not exist. Create an orphan branch.
   git checkout -b $SITE > /dev/null 2>&1
-  git add --all .
-  git commit -m "Initial commit" > /dev/null 2>&1
+  #git add --all .
+  #git commit -m "Initial commit" > /dev/null 2>&1
   echo "$SITE branch does not exist, created new"
 fi
 
@@ -52,7 +67,7 @@ git commit -m "$message" > /dev/null 2>&1
 # Delete the temporary folder
 rm -r ~/$tmp_dir
 
-# Push latest SITE to server
+# Push latest SITE to github
 git push -u origin $SITE > /dev/null 2>&1
 if [ $? = 0 ]; then
   echo "Push $SITE successful"
@@ -63,13 +78,6 @@ fi
 # Switch back to SOURCE branch
 git checkout $SOURCE > /dev/null 2>&1
 
-# Push the SOURCE to the server
-git push -u origin $SOURCE > /dev/null 2>&1
-if [ $? = 0 ]; then
-  echo "Push $SOURCE successful"
-else
-  echo "Push $SOURCE failed"
-fi
 
 
 
